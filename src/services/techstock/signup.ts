780dba34fcc8fdc71c6c3d3bd15e662eb4db4810
Daniel Gustavo <dg520589@gmail.com>
2024-10-12 17:29:52 -0300

    fix: fix signup

--- src/services/techstock/signup.ts
+++ src/services/techstock/signup.ts
@@ -4,6 +4,7 @@ import { TUser } from './login';
 export async function signUp(name: string, password: string) {
   await (() => new Promise((resolve) => setTimeout(resolve, 1000)))();
   const response = await api.post<TUser>('/users', { name, password });
+  await api.post<TUser>('/users/login', { name, password });
 
   return response.data;
 }
